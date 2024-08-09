e8ab403e3030f6a220a6e0eb5c25b65ae9d45c19
shanukafer98 <shanukafer98@gmail.com>
2024-08-09 15:56:47 +0530

    chore: Add signin backend function

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -16,3 +16,27 @@ export const signup = async (req, res) => {
     }
 
 }
+
+
+export const signin  = async (req, res) => {
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if (!validUser){
+            return next(errorHandler(404, 'User not found!'));
+        }
+        const isMatch = bycryptjs.compareSync(password, validUser.password);
+        if (!isMatch){
+            return next(errorHandler(401, 'Wrong credentials!'));
+        }
+        const {password:pass,...rest} = validUser._doc;
+
+        res.status(200).json(rest)
+    }catch(error){
+        next(error)
+    }
+
+
+
+
+}
