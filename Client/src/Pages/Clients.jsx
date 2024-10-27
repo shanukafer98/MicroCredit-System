83071c13ad3be2bf90b1f9460e07ed2b67cd8178
shanukafer98 <shanukafer98@gmail.com>
2024-10-27 22:37:48 +0530

    Refactor Clients component to remove unused import

--- Client/src/Pages/Clients.jsx
+++ Client/src/Pages/Clients.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-
 import useClientStore from "../Store/ClientStore";
 
 
